e7ececd1eaa594a5c821be59d7339cc522edaf9b
Ivang71 <artemtolv@gmail.com>
2025-11-21 12:34:57 +0300

    add nvm installation

--- setup.sh
+++ setup.sh
@@ -1,2 +1,17 @@
+sudo apt update
+
+sudo apt install curl git -y
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install --lts
+npm install -g npm@latest
+
+npm install --global yarn
+
 yarn playwright install-deps
 yarn playwright install
