32e95605f36a2fa62c29a9bcc54492669b293345
Ivang71 <artemtolv@gmail.com>
2025-11-21 12:46:22 +0300

    downgrade nvm install script version

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ sudo apt update
 
 sudo apt install curl git -y
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
